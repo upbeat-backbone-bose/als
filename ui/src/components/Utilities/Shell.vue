fe5a856e536757fa2b9848cfa1fec9d7fb6c3a13
upbeat-backbone-bose <438092+upbeat-backbone-bose@users.noreply.github.com>
2026-03-19 09:31:22 +0000

    chore: migrate xterm from v5 to @xterm/xterm v6

--- ui/src/components/Utilities/Shell.vue
+++ ui/src/components/Utilities/Shell.vue
@@ -1,6 +1,6 @@
 <script setup>
-import 'xterm/css/xterm.css'
-import { Terminal } from 'xterm'
+import '@xterm/xterm/css/xterm.css'
+import { Terminal } from '@xterm/xterm'
 import { FitAddon } from '@xterm/addon-fit'
 import { onUnmounted } from 'vue'
 import { useAppStore } from '@/stores/app'
